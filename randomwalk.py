49b6cb72ed5116cd811b292d6a63bebe6a7f9d56
Kishani Kandasamy <53715187+KishaniKandasamy@users.noreply.github.com>
2021-08-13 23:20:37 +0530

    Create randomwalk.py

---
+++ randomwalk.py
@@ -0,0 +1,16 @@
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+
+#from the origin
+X_O = np.array([[0],[0]])
+
+delta_X=np.random.normal(0,1,(2, 100))
+plt.plot(delta_X[0],delta_X[1],"go")
+
+#find cumulative sum of displacements
+X=np.cumsum(delta_X, axis=1)
+
+#concatenate numpy arrays 
+X=np.concatenate((X_O,X), axis=1)
+
