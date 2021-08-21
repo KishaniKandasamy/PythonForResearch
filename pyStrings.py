d93f7d927b7fe882b7738ef929b0c14f04ccdd8a
Kishani Kandasamy <53715187+KishaniKandasamy@users.noreply.github.com>
2021-08-22 00:09:47 +0530

    Update pyStrings.py

--- pyStrings.py
+++ pyStrings.py
@@ -1,1 +1,18 @@
-
+def countWords(text):
+    """Count the numbers of words occurs in a text"""
+    
+    text =text.lower()
+    skips=[",","!",";",":","."]
+    for ch in skips:
+        text=text.replace(ch,"")
+    
+    word_counts = {}
+    for word in text.split(" "):
+        if(word in word_counts):
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+    return word_counts
+            
+text = "I am a girl a girl lives in a village near to col"
+countWords(text)
