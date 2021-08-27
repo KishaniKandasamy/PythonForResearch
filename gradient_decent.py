8eeb2e0253523dc1435321774a8b4e02338f5703
Kishani Kandasamy <53715187+KishaniKandasamy@users.noreply.github.com>
2021-08-27 20:40:06 +0530

    add gradient_decent func

--- gradient_decent.py
+++ gradient_decent.py
@@ -10,3 +10,13 @@ def gradient_decent(x,y):
     iterations = 1000
     n = len(x) #no of data points 
     learning_rate = 0.01
+   
+       
+    for i in range(iterations):
+        y_predicted = m_curr * x + b_curr
+        cost_MSE = (1/n) * sum([val**2  for val in (y- y_predicted)])
+        md = -(2/n) * sum( x * (y - y_predicted)) # m derivative
+        bd = -(2/n) * sum(y - y_predicted) # b derivative
+        m_curr = m_curr - learning_rate * md
+        b_curr = b_curr - learning_rate * bd
+        print("m = {} , b= {} ,cost = {} iteration = {} ".format(m_curr,b_curr,cost_MSE, i))
